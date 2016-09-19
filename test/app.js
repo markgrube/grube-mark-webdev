9fa740edc44f2e956ce5ec9c92a2f879e2988903
Mark Grube <markg140@gmail.com>
2016-09-18 17:09:15 -0700

    fixed app.js

--- test/app.js
+++ test/app.js
@@ -7,11 +7,11 @@ module.exports = function(app)
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+        connectionString = process.env.MLAB_DB_URL_INIT +
+            process.env.MLAB_DB_USERNAME + ":" +
+            process.env.MLAB_DB_PASSWORD +
+            process.env.MLAB_DB_URL_END + '/' +
+            process.env.MLAB_DB_NAME;
     }
 
     var mongoose = require("mongoose");
