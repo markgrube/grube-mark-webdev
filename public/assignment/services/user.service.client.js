b2960f64764889ab867dd1345039501c286d82a4
Mark Grube <grube.m@husky.neu.edu>
2016-10-19 18:51:33 -0700

    finished user service

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     angular
         .module("WebAppMaker")
         .factory("UserService", UserService);
@@ -6,20 +6,31 @@
     function UserService() {
         var users = [
             {username: 'bjergsen', password: 'ewq', _id: 123, first: 'Soren', last: 'Bjerg', email: 'bjergsen@tsm.gg'},
-            {username: 'doublelift', password: 'ewq', _id: 234, first: 'Yilian', last: 'Peng', email: 'doublelift@tsm.gg'},
+            {
+                username: 'doublelift',
+                password: 'ewq',
+                _id: 234,
+                first: 'Yilian',
+                last: 'Peng',
+                email: 'doublelift@tsm.gg'
+            },
             {username: 'dyrus', password: 'ewq', _id: 345, first: 'Marcus', last: 'Hill', email: 'dyrus@tsm.gg'}
         ];
 
         var api = {
             findUserByCredentials: findUserByCredentials,
-            findUserById: findUserById
+            findUserById: findUserById,
+            createUser: createUser,
+            findUserByUsername: findUserByUsername,
+            updateUser: updateUser,
+            deleteUser: deleteUser
         };
         return api;
 
         function findUserById(userId) {
-            for(var u in users) {
+            for (var u in users) {
                 user = users[u];
-                if(user._id === userId) {
+                if (user._id === userId) {
                     return user;
                 }
             }
@@ -27,14 +38,54 @@
         }
 
         function findUserByCredentials(username, password) {
-            for(var u in users) {
+            for (var u in users) {
                 user = users[u];
-                if(    user.username === username
+                if (user.username === username
                     && user.password === password) {
                     return user;
                 }
             }
             return null;
+        }
+
+        function createUser(user) {
+            const lastIndex = users[users.length - 1]._id
+
+            user._id = lastIndex + 1
+            users.push(user)
+
+            return user;
+        }
+
+        function findUserByUsername(username) {
+            for (var u in users) {
+                user = users[u];
+                if (user.username === username) {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        function updateUser(userId, user) {
+            user._id = userId
+
+            for (var u in users) {
+                if (users[u]._id === userId) {
+                    users[u] = user;
+                }
+            }
+            return null;
+        }
+
+        function deleteUser(userId) {
+            for (var u in users) {
+                if (users[u]._id === userId) {
+                    users.splice(u, 1)
+                    return 0;
+                }
+            }
+            return 1;
         }
     }
 })();
